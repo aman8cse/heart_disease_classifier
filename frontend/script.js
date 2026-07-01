7593902400b58c5992d4caebad66c49a038dadd6
Aman Agrahari <aman8cse@gmail.com>
2026-07-01 21:32:44 +0530

    integrated frontend with api

--- frontend/script.js
+++ frontend/script.js
@@ -26,7 +26,7 @@ form.addEventListener("submit", async (e) => {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://heart-disease-classifier-dtsi.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
